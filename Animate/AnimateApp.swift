ffc57331299d38990e4337ea9313d38fef6f7af9
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-21 09:00:22 -0500

    Initial Commit

---
+++ Animate/AnimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimateApp.swift
+//  Animate
+//
+//  Created by Mike Chirico on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
